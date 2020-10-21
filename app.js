f082d627956e0bd4db98c49245af91c9f63ef4d5
Doxteur <Doxteurn@gmail.com>
2020-10-21 02:14:18 +0200

    Calcule nombre de joueur

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
+let numberofPlayer = 0;
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-    
+
 });
 
 io.on('connection', (socket) => {
 
+    socket.join('chambre');
+    numberofPlayer += 1;
+    console.log(numberofPlayer)
+
+
+    socket.on('disconnect', (reason) => {
+
+        numberofPlayer -= 1;
+        console.log(numberofPlayer)
+
+    });
+
     socket.on('placeAColor', (emplacement) => {
-        io.emit('placeAColor', emplacement);
-      });
+        console.log(numberofPlayer)
+
+        if (numberofPlayer >= 2) {
+            io.emit('placeAColor', emplacement);
+        }
+    });
+        io.emit('numberOfPlayer', numberofPlayer);
+
+
 
 });
 
 http.listen((process.env.PORT || 5000), () => {
-    console.log('listening on *:5000');
+    console.log('listening on localhost:5000');
 });
